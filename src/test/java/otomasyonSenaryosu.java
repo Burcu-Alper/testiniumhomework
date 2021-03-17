395425365c04b734d161907c42198a78a174e5e4
Burcu ALPER <67924975+Burcu-Alper@users.noreply.github.com>
2021-03-17 12:12:04 +0300

    sepetten silindi.

--- src/test/java/otomasyonSenaryosu.java
+++ src/test/java/otomasyonSenaryosu.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import static java.lang.Thread.sleep;
 import static org.openqa.selenium.By.*;
 
@@ -21,21 +25,34 @@ public class otomasyonSenaryosu {
         driver.manage().window().maximize();
         System.out.println("browser ekran boyuttunu ayarlama");
         driver.manage().deleteAllCookies();
-        driver.get("https://www.gittigidiyor.com/");
+
+        driver.get("https://www.gittigidiyor.com/uye-girisi");
         System.out.println("web sitesine giriş");
 
+        driver.findElement(id("L-UserNameField")).sendKeys("deneme12345test@hotmail.com");
+        driver.findElement(id("L-PasswordField")).sendKeys("Parola123_123");
+driver.findElement(cssSelector("#gg-login-enter")).click();
 
-
-        driver.findElement(By.name("k")).sendKeys("Bilgisayar");
+        driver.findElement(name("k")).sendKeys("Bilgisayar");
         System.out.println("Arama kutucuğuna bilgisayar kelimesi girilir");
 
-        driver.findElement((By.xpath("//*[@id=\"main-header\"]/div[3]/div/div/div/div[2]/form/div/div[2]/button"))).click();
+        driver.findElement((xpath("//*[@id=\"main-header\"]/div[3]/div/div/div/div[2]/form/div/div[2]/button"))).click();
         System.out.println("Bul butonu tıklanır.");
-        driver.findElement(By.xpath(".//*[@class='pager pt30 hidden-m gg-d-24']/a[2]")).click();
 
+        driver.findElement(cssSelector("#product_id_607730685 > a")).click();
+        System.out.println("ürünlerden rastgele bir ürün seçilir.");
 
-        System.out.println("debug");
+        driver.findElement(cssSelector("#buyitnow_adet")).clear();
+        driver.findElement(cssSelector("#buyitnow_adet")).sendKeys("1");
+        driver.findElement(By.id("sp-addbasket-button")).click();
+        System.out.println("Seçilen ürün sepete eklenir");
 
+        driver.findElement(By.cssSelector("#header_wrapper > div.header-icon-container.robot-header-iconContainer.gg-w-5.gg-d-6.gg-t-14.gg-m-11.gg-w-push-14.gg-d-push-12.gg-t-push-0.gg-m-push-0 > div.basket-container.robot-header-iconContainer-cart > a")).click();
+        System.out.println("Sepet butonuna tıklanır.");
+
+        driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+        driver.findElement(By.className("btn-delete")).click();
+        System.out.println("Ürün sepetten silinir.");
 
     }
     }
